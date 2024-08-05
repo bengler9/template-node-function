bad3a357018f401f7a70271b821e7cadb951261e
Brian Engler <bengler9@gmail.com>
2024-08-05 18:53:59 -0400

    first version

---
+++ index.mjs
@@ -0,0 +1,9 @@
+export const handler = async (event) => {
+    // TODO implement
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify('Good Day from CloudBot!'),
+    };
+    return response;
+  };
+  
